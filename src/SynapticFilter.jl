870c319f1147a328894b8f1583943eab48096216
Simone Carlo Surace <simsurace@gmx.net>
2021-06-22 21:04:09 +0200

    define and test performance structures

--- src/SynapticFilter.jl
+++ src/SynapticFilter.jl
@@ -3,6 +3,7 @@ module SynapticFilter
 using CUDA: AnyCuMatrix
 using Distributions: Poisson
 using LinearAlgebra: dot
+using Statistics: mean
 
 include("models.jl")
 export
@@ -17,5 +18,13 @@ include("filters.jl")
 export 
     FilterState,
     BlockSF, DiagSF, FullSF
+
+include("simulation.jl")
+export 
+    Simulation, SimulationState
+
+include("performance.jl")
+export 
+    ErrorMeasure, ComputeMSE
 
 end#module
---
+++ src/performance.jl
@@ -0,0 +1,40 @@
+abstract type ErrorMeasure end
+
+(f::ErrorMeasure)(sstate::SimulationState) = f(sstate.hstate, sstate.fstate)
+
+function (f::ErrorMeasure)(
+    imodel, smodel, omodel, filter; 
+    num_timesteps, timestep, burnin::Int = 0
+)
+    sim = Simulation(imodel, smodel, omodel, filter)
+    f(sim, num_timesteps, timestep, f, burnin)
+end
+
+function (f::ErrorMeasure)(sim::Simulation; num_timesteps, timestep, burnin::Int = 0) 
+    run(sim, num_timesteps, timestep, f, burnin)
+end
+
+function run(sim, num_timesteps, timestep, errormeasure, burnin)
+    sstate = SimulationState(sim.imodel, sim.filter)
+    total_error = 0.
+    for k in 1:num_timesteps
+        time = k * timestep
+        update!(sstate, sim, timestep, time)
+        if k > burnin
+            total_error += errormeasure(sstate)
+        end
+    end
+    return total_error / (num_timesteps - burnin)
+end
+
+struct MSE <: ErrorMeasure end
+
+ComputeMSE = MSE()
+
+function (f::MSE)(hstate::State, fstate::FilterState)
+    w = hstate.w 
+    μ = fstate.μ
+    f(w, μ)
+end
+
+(f::MSE)(w::AbstractArray, μ::AbstractArray) = mean((w .- μ).^2)
