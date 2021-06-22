04342bde93715b90b33a1aad7e9af35631fac335
Simone Carlo Surace <simsurace@gmx.net>
2021-06-22 23:27:27 +0200

    add benchmarks for ComputeMSE

--- test/performance.jl
+++ test/performance.jl
@@ -4,7 +4,7 @@
         μ = [0, -1, 4, 2]
 
         @test ComputeMSE(w, μ) ≈ (1 + 3^2 + 1) / 4
-    end#ComputeMSE
+    end
 
     @testset "MSE == prior variance" begin
         imodel = PoissonExpModel(20, 0.025, 1)
@@ -30,5 +30,58 @@
         mse = ComputeMSE(sim; num_timesteps = 100000, timestep = 0.01, burnin = 1000)
 
         @test 0.9 < mse < 1.1
-    end#ComputeMSE
+    end
+    
+    @testset "Benchmark ComputeMSE of FullSF with dim = 1024" begin
+        dim = 1024
+
+        imodel = PoissonExpModel(20, 0.025, dim)
+        smodel = OUModel(100, 1)
+        omodel = ExpGainModel(20., 0.01)
+        filter = FullSF(dim, smodel, omodel)
+
+        sim = Simulation(imodel, smodel, omodel, filter)
+
+        println("")
+        println("Benchmarking ComputeMSE of FullSF with dim = 1024")
+        display(@benchmark ComputeMSE($sim; num_timesteps = 1000, timestep = 0.001))
+        println("")
+        println("")
+    end
+
+    @testset "Benchmark ComputeMSE of BlockSF with 128 8x8 blocks" begin
+        dim = 1024
+        numblocks = 128
+        blocksize = 8
+
+        imodel = PoissonExpModel(20, 0.025, dim)
+        smodel = OUModel(100, 1)
+        omodel = ExpGainModel(20., 0.01)
+        filter = BlockSF(numblocks, blocksize, smodel, omodel)
+
+        sim = Simulation(imodel, smodel, omodel, filter)
+
+        println("")
+        println("Benchmarking ComputeMSE of BlockSF with 128 8x8 blocks")
+        display(@benchmark ComputeMSE($sim; num_timesteps = 1000, timestep = 0.001))
+        println("")
+        println("")
+    end
+
+    @testset "Benchmark ComputeMSE of DiagSF with dim = 1024" begin
+        dim = 1024
+
+        imodel = PoissonExpModel(20, 0.025, dim)
+        smodel = OUModel(100, 1)
+        omodel = ExpGainModel(20., 0.01)
+        filter = DiagSF(dim, smodel, omodel)
+
+        sim = Simulation(imodel, smodel, omodel, filter)
+
+        println("")
+        println("Benchmarking ComputeMSE of DiagSF with dim = 1024")
+        display(@benchmark ComputeMSE($sim; num_timesteps = 1000, timestep = 0.001))
+        println("")
+        println("")
+    end
 end#performance.jl
