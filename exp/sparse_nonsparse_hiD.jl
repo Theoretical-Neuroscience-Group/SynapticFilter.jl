385fa3a041520a66e537226db967261fa7472ab9
Simone Carlo Surace <simsurace@gmx.net>
2021-06-25 01:43:27 +0200

    new simulations and figures, renaming

--- exp/sparse_nonsparse_hiD.jl
+++ exp/sparse_nonsparse_hiD.jl
@@ -10,11 +10,11 @@ using SynapticFilter
 σs        = 1    # OU variance
 g0        = 20   # baseline output firing rate
 β0        = 0.01 # gain for numblocks = 1
-blocksize = 10   # size of input block
+blocksize = 8    # size of input block
 τblock    = 0.1  # duration of block activation
 
 epoch      = ceil(Int, τ)                   # duration of one epoch
-num_epochs = 16                            # number of epochs to simulate 
+num_epochs = 128                            # number of epochs to simulate 
 num_burnin = 8                              # number of epochs to burn in
 ts_per_sec = 1000                           # number of timesteps per seconds 
                                             # (reciprocal of timestep)
@@ -129,6 +129,8 @@ Threads.@threads for numblocks in numblock_rng
     resultsFullS[numblocks] = mse
 end
 
+println("
+TOTAL SIMULATION TIME: ")
 end# @time
 
 
@@ -136,29 +138,33 @@ end# @time
 # Plotting #
 ############
 
-using Plots
+using Plots; pgfplotsx()
 plot(
     resultsDiag, 
     label = "diagonal SF", 
-    color = "black", 
+    color = "black",    
     markershape = :circle,
-    legend = :bottomright
+    legend = :bottomright,
+    xscale = :log2,
+    xticks = numblock_rng,
+    ylims = (0, 1),
+    yticks = 0:0.1:1
 )
 plot!(
     resultsBlock, 
     label = "block SF", 
     color = "gray", 
-    markershape = :circle,
-    markercolor = "gray"
+    markerstrokecolor = "gray",
+    markershape = :circle
 )
 plot!(
     resultsFull, 
     label = "full SF", 
     color = "red",
-    markercolor = "red",
+    markerstrokecolor = "red",
     markershape = :circle, 
-    xlabel = "number of input blocks of size 10",
-    ylabel = "mean-squared error", 
+    xlabel = "number of input blocks of size 8",
+    ylabel = "mean-squared error" 
 )
 
 plot!(
@@ -174,15 +180,64 @@ plot!(
     color = "gray", 
     linestyle = :dash,
     markershape = :cross,
-    markercolor = "gray"
+    markerstrokecolor = "gray",
 )
 plot!(
     resultsFullS, 
     label = "full SF, sparse input", 
     color = "red",
     linestyle = :dash,
-    markercolor = "red",
+    markerstrokecolor = "red",
     markershape = :cross
 )
 
-savefig("exp/fig/plot_nonsparse_HD.png")
+savefig("exp/fig/sparse_nonsparse_hiD.png")
+
+resultsDiag[1] = 0.2601673108827467
+resultsDiag[2] = 0.3966687801368161
+resultsDiagS[1] = 0.26782323678503567
+resultsDiag[4] = 0.49670215028163955
+resultsDiagS[2] = 0.40597316888846924
+resultsDiagS[4] = 0.5679283192541236
+resultsDiag[8] = 0.6424592953818584
+resultsDiagS[8] = 0.7681252953006376
+resultsDiagSF[16] = 0.7552139548551479
+resultsDiagS[16] = 0.8838554924651764
+resultsDiag[32] = 0.8548628511695536
+resultsDiagS[32] = 0.9561353460554867
+resultsDiag[64] = 0.9126570741163882
+resultsDiagS[64] = 0.9836768689299181
+resultsDiag[128] = 0.9550061678789515
+resultsDiagS[128] = 0.9954426494747298
+resultsBlock[1] = 0.2156723007351738
+resultsBlock[2] = 0.3198066644728403
+resultsBlockS[1] = 0.1862129671305838
+resultsBlockS[2] = 0.34697504267143275
+resultsBlock[4] = 0.418177739260888
+resultsBlockS[4] = 0.5369980767528025
+resultsBlock[8] = 0.5320928780895958
+resultsBlockS[8] = 0.7395659863037587
+resultsBlock[16] = 0.6351371101648544
+resultsBlockS[16] = 0.8861266619256182
+resultsBlock[32] = 0.7639850211049789
+resultsBlockS[32] = 0.9605318488554098
+resultsBlock[64] = 0.8531556090570227
+resultsBlockS[64] = 0.9848938168160051
+resultsBlock[128] = 0.9132110252540951
+resultsBlockS[128] = 0.9935509726243488
+resultsFull[1] = 0.20142420879646247
+resultsFull[2] = 0.2742953790599208
+resultsFullS[1] = 0.18646626871282518
+resultsFullS[2] = 0.33465317296387664
+resultsFull[4] = 0.37244410525023736
+resultsFullS[4] = 0.519013327567376
+resultsFull[8] = 0.49283225014717347
+resultsFullS[8] = 0.7503495919688189
+resultsFull[16] = 0.6069734723574606
+resultsFullS[16] = 0.8953430874335794
+resultsFull[32] = 0.704067043722794
+resultsFullS[32] = 0.9579965597731406
+resultsFull[64] = 0.7887769848033332
+resultsFullS[64] = 0.986572562003516
+resultsFull[128] = 0.8826750416811971
+resultsFullS[128] = 0.999694821910929
